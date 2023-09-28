ba356bd86b9799a5e0ad841acac435401738edd1
SamAndrew81 <42827688+SamAndrew81@users.noreply.github.com>
2023-09-27 21:16:38 -0600

    Update app1.py

--- app1/app1.py
+++ app1/app1.py
@@ -5,7 +5,7 @@ app1 = Flask(__name__)
 
 @app1.route('/')
 def hello_world():
-    return ' This is App1 '
+    return ' This is App-1 '
 
 if __name__ == '__main__':
    app1.run(debug=True, host='0.0.0.0')
